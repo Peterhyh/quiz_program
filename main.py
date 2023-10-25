a10a2379a6b923b7965be333ed69b9659d48b7da
Peter Huynh <peterhyh@yahoo.com>
2023-10-25 18:36:58 -0500

    creating first object from class

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-class question:
-    def __init__ (self, ):
-        pass
+class Question:
+    def __init__ (self, name ):
+        self.name = name
+        self.score = 0
+
+
+player_one = Question('Peter')
+
+print (player_one.name)

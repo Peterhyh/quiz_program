aacb976899d34b212f7c408d35fd543eedfe5bbf
Peter Huynh <peterhyh@yahoo.com>
2023-10-26 15:20:50 -0500

    finished quiz

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from questions_model import Question
 from data import question_data
+from quiz_logic import QuizLogic
 
 question_bank = []
 
@@ -9,4 +10,10 @@ for question in question_data:
     new_question = Question(text = question_text, answer = question_answer)
     question_bank.append(new_question)
 
-print(question_bank[0].text)
+quiz = QuizLogic(question_list = question_bank)
+
+
+while quiz.more_question():
+    quiz.next_question()
+
+print(f'You got {quiz.number_of_correct_answers}/{quiz.question_number} correct!')
---
+++ quiz_logic.py
@@ -0,0 +1,20 @@
+class QuizLogic:
+
+    def __init__(self, question_list):
+        self.question_number = 0
+        self.question_list = question_list
+        self.number_of_correct_answers = 0
+
+    def more_question(self):
+        return self.question_number < len(self.question_list)
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        user_answer = input(f'Q.{self.question_number}: {current_question.text}. (True/False)?: ')
+        if user_answer == current_question.answer:
+            print('You got it right!')
+            self.number_of_correct_answers += 1
+        else:
+            print('Wrong')
+        print(f'The correct answer was: {current_question.answer}')
